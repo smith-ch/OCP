d817cc18cb9ac1828ab012a17afba5e4505b05bd
Smith Rodriguez <20231123@itla.edu.do>
2024-07-30 15:52:59 -0400

    actualizaciones

---
+++ OCP/OCP/Models/Course.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ProyectoCursos.Models
+{
+    public abstract class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+
+        public abstract void Subscribe(Student std);
+    }
+}
